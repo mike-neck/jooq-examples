477c0298b8b8cc9004647ad91f63c5c032191bbf
mike-neck <s.mochida@quo-digital.jp>
2020-07-10 21:24:35 +0900

    add test to TemporaryUserRepository

---
+++ jooq-sns-example/app/src/test/kotlin/com/example/temporaryusers/TemporaryUserRepositoryTest.kt
@@ -0,0 +1,85 @@
+package com.example.temporaryusers
+
+import com.example.Now
+import com.example.TableFactory
+import com.example.UsingDatabase
+import com.example.emails.EmailAddress
+import com.example.emails.EmailId
+import com.example.emails.EmailRepository
+import com.example.emails.EmailRepositoryTest
+import db.fixture.TestSetup
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.db.api.Assertions.assertThat
+import org.jooq.DSLContext
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
+import org.springframework.transaction.support.TransactionTemplate
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
+import java.time.temporal.ChronoUnit
+import kotlin.test.Test
+
+@UsingDatabase
+@Import(
+    TemporaryUserRepositoryTest.Config::class,
+    EmailRepositoryTest.Config::class)
+class TemporaryUserRepositoryTest {
+
+    @Autowired
+    lateinit var testSetup: TestSetup
+
+    @Autowired
+    lateinit var transactionTemplate: TransactionTemplate
+
+    @Suppress("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    lateinit var emailRepository: EmailRepository
+
+    @Autowired
+    lateinit var temporaryUserRepository: TemporaryUserRepository
+
+    @Autowired
+    lateinit var tableFactory: TableFactory
+
+    @BeforeEach
+    fun setup() {
+        testSetup.clean()
+    }
+
+    @Test
+    fun save() {
+        val instant = OffsetDateTime.of(2020, 1, 2, 15, 4, 5, 6, ZoneOffset.UTC).toInstant()
+        val now = Now(instant)
+        val user = transactionTemplate.execute {
+            val emailId = EmailId(200L)
+            emailRepository.save(emailId, EmailAddress("test", "example.com"), now)
+            val temporaryUser = TemporaryUser(TemporaryHashKey("22bb33cc44dd"), emailId, TemporaryUserCreated(instant))
+            temporaryUserRepository.save(temporaryUser)
+        }
+
+        assertThat(user)
+            .isNotNull
+            .satisfies { assertThat(it?.emailId).isEqualTo(EmailId(200L)) }
+            .satisfies { assertThat(it?.created).isEqualTo(TemporaryUserCreated(instant.truncatedTo(ChronoUnit.MILLIS))) }
+
+        val temporaryUsers = tableFactory.table("temporary_users")
+
+        assertThat(temporaryUsers)
+            .column("temporary_hash_key")
+            .value().isEqualTo("22bb33cc44dd")
+            .column("email_id")
+            .value().isEqualTo(200L)
+    }
+
+    @Configuration
+    class Config {
+
+        @Suppress("SpringJavaInjectionPointsAutowiringInspection")
+        @Bean
+        fun temporaryUserRepository(dsl: DSLContext): TemporaryUserRepository =
+            TemporaryUserRepositoryImpl(dsl)
+    }
+}
