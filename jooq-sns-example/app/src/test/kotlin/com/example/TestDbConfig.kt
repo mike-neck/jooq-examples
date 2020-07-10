a2ad42836e0a47f7d69ec42792866cb27b110f20
mike-neck <s.mochida@quo-digital.jp>
2020-07-10 20:42:18 +0900

    add test to EmailRepository

--- jooq-sns-example/app/src/test/kotlin/com/example/TestDbConfig.kt
+++ jooq-sns-example/app/src/test/kotlin/com/example/TestDbConfig.kt
@@ -2,6 +2,7 @@ package com.example
 
 import com.zaxxer.hikari.HikariDataSource
 import db.fixture.TestSetup
+import org.assertj.db.type.Table
 import org.jooq.DSLContext
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
@@ -56,4 +57,14 @@ class TestDbConfig {
     @Bean
     fun testSetup(dsl: DSLContext, transactionTemplate: TransactionTemplate, jdbcTemplate: JdbcTemplate): TestSetup =
         TestSetup(dsl, transactionTemplate, jdbcTemplate)
+
+    @Bean
+    fun tableFactory(dataSource: DataSource): TableFactory =
+        object : TableFactory {
+            override fun table(name: String): Table = Table(dataSource, name)
+        }
+}
+
+interface TableFactory {
+    fun table(name: String): Table
 }
---
+++ jooq-sns-example/app/src/test/kotlin/com/example/emails/EmailRepositoryTest.kt
@@ -0,0 +1,63 @@
+package com.example.emails
+
+import com.example.Now
+import com.example.TableFactory
+import com.example.UsingDatabase
+import db.fixture.TestSetup
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.db.api.Assertions.assertThat
+import org.jooq.DSLContext
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
+import org.springframework.transaction.support.TransactionTemplate
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
+import kotlin.test.Test
+
+@UsingDatabase
+@Import(EmailRepositoryTest.Config::class)
+class EmailRepositoryTest {
+
+    @Autowired
+    lateinit var testSetup: TestSetup
+
+    @Autowired
+    lateinit var transactionTemplate: TransactionTemplate
+
+    @Autowired
+    lateinit var emailRepository: EmailRepository
+
+    @Autowired
+    lateinit var tableFactory: TableFactory
+
+    @BeforeEach
+    fun setup() {
+        testSetup.clean()
+    }
+
+    @Test
+    fun save() {
+        val instant = OffsetDateTime.of(2020, 1, 2, 15, 4, 5, 0, ZoneOffset.UTC).toInstant()
+        val now = Now(instant)
+        val emailId = transactionTemplate.execute {
+            emailRepository.save(EmailId(200), EmailAddress("user-1", "example.com"), now)
+        }
+        assertThat(emailId).isNotNull
+        val id = emailId as EmailId
+        val emails = tableFactory.table("emails")
+        assertThat(emails)
+            .column("email_id")
+            .value().isEqualTo(id.value)
+    }
+
+    @Configuration
+    class Config {
+
+        @Suppress("SpringJavaInjectionPointsAutowiringInspection")
+        @Bean
+        fun emailRepository(dsl: DSLContext): EmailRepository = EmailRepositoryImpl(dsl)
+    }
+}
